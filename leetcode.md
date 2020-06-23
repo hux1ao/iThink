bad14c4bae9ff4df0853163fd093645f30bfea07
xiao.hu <xiao.hu@11bee.com>
2020-06-23 12:21:37 +0800

    面试

--- leetcode.md
+++ leetcode.md
@@ -4,3 +4,19 @@
 
 ## 回溯
 [括号生成](https://leetcode-cn.com/problems/generate-parentheses/)
+
+## 二叉树遍历
+[二叉树的锯齿形层次遍历](https://leetcode-cn.com/problems/binary-tree-zigzag-level-order-traversal/)
+
+[对称二叉树](https://leetcode-cn.com/problems/symmetric-tree/submissions/)
+
+
+## 单调栈
+```// todo```
+[每日温度](https://leetcode-cn.com/problems/daily-temperatures/solution/mei-ri-wen-du-by-leetcode-solution/)
+
+## hash表
+[字母异位词分组](https://leetcode-cn.com/problems/group-anagrams/)
+
+## 贪心算法
+[买卖股票的最佳时机 II](https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-ii/submissions/)
--- practice.js
+++ practice.js
@@ -272,3 +272,66 @@ function foo (arr1, arr2) {
 
 
 // git rebase
+
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+    const result = {}
+    strs.forEach(item => {
+        const itemSorted = item.split('').sort().join('');
+        if (result[itemSorted]) {
+            result[itemSorted].push(item);
+        } else {
+            result[itemSorted] = [item]
+        }
+    })
+    return Object.values(result).sort((next, pre) => {
+        return pre.length < next.length
+    });
+};
+
+
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function(s) {
+    if (!s) {
+        return 0
+    }
+    if (s.length === 1) {
+        return 1;
+    }
+    let result = 0;
+    const dp = Array.from({length: s.length}).map((item) => {
+        return []
+    });
+    for (let x = 0; x < s.length; x++) {
+        for (let y = x; y < s.length; y ++) {
+            if (x === y) {
+                dp[x][y] = 1;
+            } else {
+                if (s[x] === s[y]) {
+                    if (y - x === 2) {
+                        dp[x][y] = 3;
+                    } else {
+                        dp[x][y] = dp[x + 1][y - 1] + 2;
+                    }
+                } else {
+                    if (y - x === 1) {
+                        dp[x][y] = 1;
+                    } else {
+                        dp[x][y] = dp[x + 1][y - 1];
+                    }
+                }
+            }
+        }
+    }
+    console.log(dp)
+};
+
+
+console.log(longestPalindrome('babad'));
